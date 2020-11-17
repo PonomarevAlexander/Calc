0d884425b2111667798ef8bdca3e0749c54ccfe8
PonomarevAlexander <ponpmarev.alex.1992@gmail.com>
2020-11-18 00:35:45 +0300

    add Calculator class which implementing action

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+package com.ponomarev.calculator;
+
+public class Calculator {
+    public int toAdd(int a, int b) {
+        return a + b;
+    }
+    public double toAdd(double a, double b) {
+        return a + b;
+    }
+    public int toSubstruct(int a, int b) {
+        return a - b;
+    }
+    public double toSubstruct(double a, double b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
